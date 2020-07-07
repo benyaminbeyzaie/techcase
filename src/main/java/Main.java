d1bb87de9ff19b0aa73ece2309a08d44fdb2986f
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-07 18:09:53 +0430

    create new program on main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        new Program(3, 3, new File("src/main/resources/input.txt")).execute();
 
     }
 }
