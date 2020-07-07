07e8ea5c44cf14ac41fe1003a0cca3af0b42dc22
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-07 15:26:40 +0430

    Add producer methods

--- src/main/java/producer/Producer.java
+++ src/main/java/producer/Producer.java
@@ -1,4 +1,40 @@
 package producer;
 
-public class Producer {
+import broker.MessageBroker;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Producer extends Thread {
+    String name;
+    MessageBroker messageBroker;
+    File file;
+    Scanner scanner;
+    private static final Object lock = new Object();
+
+    public Producer(String name, MessageBroker messageBroker, File file) throws FileNotFoundException {
+        this.name = name;
+        this.messageBroker = messageBroker;
+        this.file = file;
+        scanner = new Scanner(file);
+    }
+
+    @Override
+    public synchronized void run() {
+        synchronized (lock){
+            try {
+                while (scanner.hasNext()){
+                    push(scanner.nextLine());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void push(String message) throws IOException {
+        messageBroker.push(name, message);
+    }
 }
