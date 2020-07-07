584e146b080831a1358a79fa8ea1cabfe04c0983
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-07 15:07:43 +0430

    Update file path

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -1,5 +1,5 @@
 package constants;
 
 public class Constants {
-    public final static String FILE_PATH = "src/main/resources/queue.txt";
+    public final static String FILE_PATH = "src/main/resources/queue";
 }
