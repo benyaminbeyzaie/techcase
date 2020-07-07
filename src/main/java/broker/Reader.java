dde6fb01bb357edf7f32dd6c85d5d5366521c1cc
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-07 15:00:09 +0430

    Add Reader methods

--- src/main/java/broker/Reader.java
+++ src/main/java/broker/Reader.java
@@ -1,4 +1,17 @@
 package broker;
 
-public class Reader {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class Reader {
+    private File queue;
+    Reader(File queue){
+        this.queue = queue;
+    }
+
+    String pullMessage() throws FileNotFoundException {
+        Scanner scanner = new Scanner(queue);
+        return scanner.nextLine();
+    }
 }
